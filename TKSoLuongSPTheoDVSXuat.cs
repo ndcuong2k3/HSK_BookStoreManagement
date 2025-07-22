43a885c049a4fb79ac9dbcdf7213fa3fcd1aead0
ndcuong2k3 <21a100100051@students.hou.edu.vn>
2025-07-22 21:55:41 +0700

    Add TKSoLuongSPTheoDVSX Report

---
+++ TKSoLuongSPTheoDVSXuat.cs
@@ -0,0 +1,27 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace HSK_BookStoreManagement
+{
+    public partial class TKSoLuongSPTheoDVSXuat : Form
+    {
+        DBHelper dbHelper = new DBHelper(); 
+        public TKSoLuongSPTheoDVSXuat()
+        {
+            InitializeComponent();
+        }
+
+        private void TKSoLuongSPTheoDVSXuat_Load(object sender, EventArgs e)
+        {
+            ReportDocument rpt = new ReportDocument();
+
+            rpt.Load("TKSoLuongSPTheoDVSX.rpt");
+            DataTable dt = dbHelper.ExecuteQuery("prThongKeSLSanPhamTheoDVSX", null, CommandType.StoredProcedure);
+
+            rpt.SetDataSource(dt);
+            crystalReportViewer1.ReportSource = rpt;
+        }
+    }
+}
